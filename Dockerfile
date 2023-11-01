cdbeb1e2e588261cf4b296158e6fd0c20e6daf51
backster <backstercorp@gmail.com>
2023-11-01 18:34:57 +0100

    configure script for build and push image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.10.13-alpine
+FROM python:3.10.13
 
 
 ENV PIP_ROOT_USER_ACTION=ignore
@@ -7,11 +7,17 @@ ARG user_login
 
 ENV USER_LOGIN=${user_login}
 
-RUN apt-get update apt-get install -y htop \
-make git  && apt-get autoremove
+RUN apt-get update \
+    && apt-get install -y htop \
+    make \
+    git \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 
 RUN python -m pip install Flask
 
 WORKDIR /backster/app
 
 COPY . /backster/app/
+
+CMD ["python", "./main.py"]
--- main.sh
+++ main.sh
@@ -1,2 +1,22 @@
-docker build --build-arg user_login="backster" rocket_star:version_1
-docker push backster/rocket_start:version_1
+#!/bin/bash
+set -e
+echo "Start build docker image"
+docker build \
+        -t "rocket-star:$(git rev-parse HEAD)" \
+        -t "rocket-star:latest" \
+        --build-arg user_login="backster"  .
+
+echo "Docker image builded"
+docker images
+
+echo "Login to Docker hub"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+echo "Start pushing image"
+
+docker push "rocket-star:latest"
+docker push "rocket-star:$(git rev-parse HEAD)"
+
+echo "Image pushed. OK"
+
+
